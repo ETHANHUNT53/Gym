9453f448d43934afa1d17e234b6dbca2dd5a5f01
PavithraBusipalli <busipallipavitra@gmail.com>
2024-08-29 11:09:42 +0530

    update-1

--- Frontend/GymManagement/src/App.jsx
+++ Frontend/GymManagement/src/App.jsx
@@ -1,10 +1,6 @@
 import "./App.css";
-import Registration from "./components/Registration";
+import Registration from "./Components/Registration";
 import Login from "./components/Login";
-import Navbar from "./components/Navbar";
-import Greeting from "./components/Greeting";
-import WorkoutCard from "./components/WorkoutCard";
-import UserProfile from "./components/UserProfile";
 import { Container, Grid } from "@mui/material";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
